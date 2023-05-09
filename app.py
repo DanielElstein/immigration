5259bb249424fc3f4ad5a14f4b340ba5a8592f0e
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-09 15:48:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 import streamlit as st
-import openai
-
-# Get OpenAI API key
-openai_api_key = st.secrets["OPENAI_API_KEY"]
-openai.api_key = openai_api_key
-
 from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
 
-data = loader.load()
+PINECONE_API_ENV = 'us-west1-gcp-free'
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-print (f'You have {len(data)} document(s) in your data')
-print (f'There are {len(data[0].page_content)} characters in your document')
+# Initialize Pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_API_ENV  # next to api key in console
+)
+index_name = "langchaintest2"
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(data)
+st.title("Document Search")
+query = st.text_input("Enter your query:")
+
+if query:
+    docs = docsearch.similarity_search(query, include_metadata=True)
+    st.header("Search Results")
+    for idx, doc in enumerate(docs):
+        st.subheader(f"Result {idx + 1}:")
+        st.write(doc.page_content[:250])
