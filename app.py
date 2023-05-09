c4b13dae615b2be30a0fd477b314aa2166cbf8a8
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-09 16:42:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,33 +5,25 @@ from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 
-try:
-    OPENAI_API_KEY = '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'
-PINECONE_API_KEY = '0afecd30-9c49-4556-900d-be421b5dcddf'
-PINECONE_API_ENV = 'us-west1-gcp-free'
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+PINECONE_API_ENV = st.secrets["PINECONE_API_ENV"]
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-    st.write("API keys loaded successfully")
+# Initialize Pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_API_ENV  # next to api key in console
+)
+index_name = "langchaintest2"
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+st.title("Document Search")
+query = st.text_input("Enter your query:")
 
-    # Initialize Pinecone
-    pinecone.init(
-        api_key=PINECONE_API_KEY,  # find at app.pinecone.io
-        environment=PINECONE_API_ENV  # next to api key in console
-    )
-    index_name = "langchaintest2"
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
-
-    st.title("Document Search")
-    query = st.text_input("Enter your query:")
-
-    if query:
-        docs = docsearch.similarity_search(query, include_metadata=True)
-        st.header("Search Results")
-        for idx, doc in enumerate(docs):
-            st.subheader(f"Result {idx + 1}:")
-            st.write(doc.page_content[:250])
-
-except Exception as e:
-    st.error(f"An error occurred: {str(e)}")
+if query:
+    docs = docsearch.similarity_search(query, include_metadata=True)
+    st.header("Search Results")
+    for idx, doc in enumerate(docs):
+        st.subheader(f"Result {idx + 1}:")
+        st.write(doc.page_content[:250])
