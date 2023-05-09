420c4a357eb5b564b822ed1342e98bcccf1935ed
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-09 16:17:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,32 @@ from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-PINECONE_API_ENV = st.secrets["PINECONE_API_ENV"]
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+try:
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+    PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+    PINECONE_API_ENV = st.secrets["PINECONE_API_ENV"]
 
-# Initialize Pinecone
-pinecone.init(
-    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
-    environment=PINECONE_API_ENV  # next to api key in console
-)
-index_name = "langchaintest2"
-docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    st.write("API keys loaded successfully")
 
-st.title("Document Search")
-query = st.text_input("Enter your query:")
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-if query:
-    docs = docsearch.similarity_search(query, include_metadata=True)
-    st.header("Search Results")
-    for idx, doc in enumerate(docs):
-        st.subheader(f"Result {idx + 1}:")
-        st.write(doc.page_content[:250])
+    # Initialize Pinecone
+    pinecone.init(
+        api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+        environment=PINECONE_API_ENV  # next to api key in console
+    )
+    index_name = "langchaintest2"
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+    st.title("Document Search")
+    query = st.text_input("Enter your query:")
+
+    if query:
+        docs = docsearch.similarity_search(query, include_metadata=True)
+        st.header("Search Results")
+        for idx, doc in enumerate(docs):
+            st.subheader(f"Result {idx + 1}:")
+            st.write(doc.page_content[:250])
+
+except Exception as e:
+    st.error(f"An error occurred: {str(e)}")
