10983301b2b4cf89729dd0f8451473e231f1f76b
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-09 16:28:32 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,23 @@ import openai
 import streamlit as st
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-
 openai.api_key = OPENAI_API_KEY
 
-# Test the API by listing models
+# Test the API with a prompt completion
 try:
-    models = openai.Model.list()
-    st.write(models)
+    prompt = "Translate the following English text to French: 'Hello, how are you?'"
+    model_engine = "text-davinci-codex-002"
+
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    st.write(response.choices[0].text.strip())
+
 except Exception as e:
     st.error(f"An error occurred with the OpenAI API: {str(e)}")
