f1f9a095b95c1770505811135325dff68bd8039a
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-10 15:09:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 import streamlit as st
-from langchain.document_loaders import UnstructuredHTMLLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma, Pinecone
+from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
 import pinecone
 
-# Initialize the memory
-memory = ConversationBufferMemory()
-
 st.set_page_config(page_title="Immigration Q&A", layout="wide", initial_sidebar_state="expanded")
 
 st.header("Immigration Q&A")
-
 
 custom_css = """
 <style>
@@ -31,14 +25,11 @@ custom_css = """
 """
 st.markdown(custom_css, unsafe_allow_html=True)
 
-# Create two columns: one for the image and one for the text
 col1, col2 = st.columns([1, 3])
 
-# Display the image in the left column
 image_path = 'statue.png'
 col1.image(image_path, width=350)
 
-# Display the text in the right column
 col2.markdown("""
 Ask a question about immigration to the United States in any language, and our artificial intelligence will answer based on the [USCIS policy manual](https://www.uscis.gov/policy-manual).
 
@@ -55,6 +46,7 @@ Báa ìbéèrè nípa ìjìmìn-ìlú sí Amẹ́ríkà ní èdè kankan, kí à
 *Legal Disclaimer: This platform is meant for informational purposes only. It is not affiliated with USCIS or any other governmental organization, and is not a substitute for professional legal advice. The answers provided are based on the USCIS policy manual and may not cover all aspects of your specific situation. For personalized guidance, please consult an immigration attorney.*
 
 """)
+
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 PINECONE_API_ENV = st.secrets["PINECONE_API_ENV"]
@@ -64,33 +56,35 @@ pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 index_name = "langchaintest2"
 docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
-# Create a form
+memory = ConversationBufferMemory(return_messages=False)
+
 with st.form(key="my_form"):
     query = st.text_input("Enter your question:")
     submit_button = st.form_submit_button("Submit")
 
 if submit_button:
+    llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
+    conversation = ConversationChain(
+        llm=llm, verbose=True, memory=memory
+    )
+
     template = """
     System: Play the role of a friendly immigration lawyer. Respond to questions in detail, in the same language as the human's most recent question. If they ask a question in Spanish, you should answer in Spanish. If they ask a question in French, you should answer in French. And so on, for every language.
-   
+    
     {conversation_text}
     
     Human: {query}
 
     Lawyer: """
-
+    
     if query:
-        prompt = template.format(query=query, conversation_text=memory.get())
-        docs = docsearch.similarity_search(query, include_metadata=True)
-
-        llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
-        conversation = ConversationChain(llm=llm, verbose=True, memory=memory)
-
+        conversation_text = memory.load_memory_variables({})['history']
+        prompt = template.format(query=query, conversation_text=conversation_text)
+        
         with st.spinner('Processing your question...'):
             result = conversation.predict(input=prompt)
 
         st.header("Answer")
         st.write(result)
-        memory.append('Human', query)
-        memory.append('Lawyer', result)
 
+        memory.save_context({"input": f"Human: {query}\n\n"}, {"output": f"Lawyer: {result}\n\n"})
