e2aba9d1a6dddec59d4706b02c78aef48f021ca0
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-10 08:42:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -43,20 +43,20 @@ st.markdown("""
 *Legal Disclaimer: This platform is not a substitute for professional legal advice. The answers provided are based on the USCIS handbook and may not cover all aspects of your specific situation. For personalized guidance, please consult an immigration attorney.*
 """)
 
-    multilingual_message = """
-    Ask a question about immigration to the United States in any language, and our artificial intelligence will answer based on the USCIS handbook.
+multilingual_message = """
+Ask a question about immigration to the United States in any language, and our artificial intelligence will answer based on the USCIS handbook.
 
-    Haga una pregunta sobre inmigración a los Estados Unidos en cualquier idioma y nuestra inteligencia artificial responderá según el manual del USCIS.
+Haga una pregunta sobre inmigración a los Estados Unidos en cualquier idioma y nuestra inteligencia artificial responderá según el manual del USCIS.
 
-    用任何语言提问有关美国移民的问题，我们的人工智能将根据美国公民及移民服务局的手册进行回答。
+用任何语言提问有关美国移民的问题，我们的人工智能将根据美国公民及移民服务局的手册进行回答。
 
-    Magtanong tungkol sa imigrasyon sa Estados Unidos sa anumang wika, at sasagutin ng aming artificial intelligence batay sa USCIS handbook.
+Magtanong tungkol sa imigrasyon sa Estados Unidos sa anumang wika, at sasagutin ng aming artificial intelligence batay sa USCIS handbook.
 
-    Hỏi về nhập cư vào Hoa Kỳ bằng bất kỳ ngôn ngữ nào, và trí thông minh nhân tạo của chúng tôi sẽ trả lời dựa trên sách hướng dẫn của USCIS.
+Hỏi về nhập cư vào Hoa Kỳ bằng bất kỳ ngôn ngữ nào, và trí thông minh nhân tạo của chúng tôi sẽ trả lời dựa trên sách hướng dẫn của USCIS.
 
-    Báa ìbéèrè nípa ìjìmìn-ìlú sí Amẹ́ríkà ní èdè kankan, kí àṣà ìmọ̀ ọ̀rọ̀ wa ṣe àpèsè láti ìwé kikọ USCIS.
-    """
-    st.write(multilingual_message)
+Báa ìbéèrè nípa ìjìmìn-ìlú sí Amẹ́ríkà ní èdè kankan, kí àṣà ìmọ̀ ọ̀rọ̀ wa ṣe àpèsè láti ìwé kikọ USCIS.
+"""
+st.write(multilingual_message)
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
