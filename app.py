03b789593a4cec7cf806a1bcd621953445d55001
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-10 15:24:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -79,13 +79,7 @@ if submit_button:
 
     Lawyer: """
 
-    if query:
-        prompt = template.format(query=query, conversation_text=memory.load_memory_variables({})['history'])
-        docs = docsearch.similarity_search(query, include_metadata=True)
-
-        llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
-        conversation = ConversationChain(
-            llm=llm, verbose=True, memory
+    
 if query:
     prompt = template.format(query=query, conversation_text=memory.load_memory_variables({})['history'])
     docs = docsearch.similarity_search(query, include_metadata=True)
