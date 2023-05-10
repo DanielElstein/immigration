d87e5de4c8cf186e598f40ecc270d0df45ecf606
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-09 20:24:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if query:
     from langchain.llms import OpenAI
     from langchain.chains.question_answering import load_qa_chain
 
-    llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model=gpt-3.5-turbo)
+    llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
     chain = load_qa_chain(llm, chain_type="stuff")
 
     result = chain.run(input_documents=docs, question=query)
