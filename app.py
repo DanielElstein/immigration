9390d524b2b8c2c7d10a28f6c4c994a876182ebf
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-11 14:30:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -72,15 +72,7 @@ if query:
     if "conversation_memory" not in st.session_state:
         st.session_state.conversation_memory = ConversationBufferMemory()
 
-    template = """
-    System: Play the role of a friendly immigration lawyer. Respond to questions in detail, in the same language as the human's most recent question. If they ask a question in Spanish, you should answer in Spanish. If they ask a question in French, you should answer in French. And so on, for every language.
-   
-    {conversation_text}
     
-    Human: {query}
-
-    Lawyer: """
-
     prompt = template.format(query=query, conversation_text=st.session_state.conversation_memory.load_memory_variables({})['history'])
 
     docs = docsearch.similarity_search(query, include_metadata=True)
