594e676d60b881191de0bd983cc00706a1c126d6
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-12 12:39:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ if query:
     Human: {human_input}
     Chatbot:"""
 
-    prompt = promptTemplate(
+    prompt = PromptTemplate(
         input_variables=["chat_history", "human_input"], 
         template=template
     )
