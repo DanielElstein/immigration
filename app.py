7850d8b7dd61146d7efea77e51b9033a9bc41015
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-12 14:11:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,10 @@ if query:
         st.session_state.conversation_memory = ConversationBufferMemory()
 
     template = """
-    {conversation_text}  
-    {query}
+    system message: play the role of a friendly immigration lawyer, who answers in the same language as the question
+    conversation history: {conversation_text}  
+    human: {query}
+    ai: 
     """
 
     # Retrieve conversation history from the memory
