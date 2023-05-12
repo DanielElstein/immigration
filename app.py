1133df2131661e0e5b6826fb48f83d32386b2762
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-12 13:05:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ if query:
         st.session_state.conversation_memory.load_memory_variables({})['input'] = query
 
         # Generate prompt with updated conversation history
-        prompt = template.format(query=query, conversation_text=st.session_state.conversation_memory.load_memory_variables({})['history'])
+        prompt = template.format(query=query, result=result, conversation_text=st.session_state.conversation_memory.load_memory_variables({})['history'])
 
         result = conversation.predict(input=prompt)
 
