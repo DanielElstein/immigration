5d1090f689719f88f03e0275fca13d1a2365030e
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-12 22:19:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Pinecone
+from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 import pinecone
 
 class Conversation:
@@ -68,66 +67,41 @@ if query:
     # Add the user's message to the conversation history
     st.session_state.conversation.add_message('Human', query)
 
+    template = """
+    System: Play the role of a friendly immigration lawyer. Respond to questions in detail, in the same language as the human's most recent question. If they ask a question in Spanish, you should answer in Spanish. If they ask a question in French, you should answer in French. And so on, for every language.
+   
+    {conversation_text}  
+    """
+
     # Retrieve the conversation history from the session state
     conversation_text = st.session_state.conversation.get_conversation()
 
+    # Generate prompt with updated conversation history
+    prompt = template.format(conversation_text=conversation_text)
+
     llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
-
-    # Perform document search
-    docs = docsearch.similarity_search(query, include_metadata=True, k=3)
-
-    
-    # Initialize memory
     memory = ConversationBufferMemory()
+    conversation = ConversationChain(llm=llm, verbose=True, memory=memory)
 
-    # Update memory with conversation_text
-    # memory.update(conversation_text)
-
-    # Load the question-answering chain
-    chain = load_qa_with_sources_chain(llm, chain_type="stuff", memory=memory)  # Replace "stuff" with the actual chain type
-
-    from langchain.prompts import (
-    ChatPromptTemplate, 
-    MessagesPlaceholder, 
-    SystemMessagePromptTemplate, 
-    HumanMessagePromptTemplate
-    )
-    from langchain.chains import ConversationChain
-    from langchain.memory import ConversationBufferMemory
-
-    # Define a prompt template
-    prompt = ChatPromptTemplate.from_messages([
-        SystemMessagePromptTemplate.from_template("Play the role of a friendly immigration lawyer. Respond to questions in detail, in the same language as the human's most recent question. If they ask a question in Spanish, you should answer in Spanish. If they ask a question in French, you should answer in French. And so on, for every language."),
-        MessagesPlaceholder(variable_name="history"),
-        HumanMessagePromptTemplate.from_template("{input}")
-    ])
-
-    # Initialize memory
-    memory = ConversationBufferMemory(return_messages=True)
-
-    # Initialize the conversation chain
-    conversation_chain = ConversationChain(memory=memory, prompt=prompt, llm=llm)
-
-    # Perform document search
-    docs = docsearch.similarity_search(query, include_metadata=True, k=3)
-
-    # Add search results to the memory
-    for doc in docs:
-        memory.add_message("System", f"Document Content: {doc.page_content}")
-
-    # Add the user's message to the memory
-    memory.add_message('Human', query)
-
-    # Run the conversation chain
     with st.spinner('Processing your question...'):
-        result = conversation_chain.run(input=query)
+        #result = conversation.predict(input=prompt)
+        result = chain.run(input_documents=docs, question=prompt)
 
     # Add the AI's response to the conversation history
-    memory.add_message('AI', result)
+    st.session_state.conversation.add_message('AI', result)
 
-    # Display the AI-generated answer
+    st.header("Prompt")
+    st.write(prompt)  # Display the prompt value
+
     st.header("Answer")
-    st.write(result)
+    st.write(result)  # Display the AI-generated answer
+
+    docs = docsearch.similarity_search(query, include_metadata=True,k=3)
+
+    # Debug: Check the docs variable
+    print(f"Number of docs: {len(docs)}")
+    for i, doc in enumerate(docs):
+        print(f"Doc {i}: {doc.page_content}")
 
     # Display search results
     if docs:
@@ -135,8 +109,13 @@ if query:
         st.write(f"Total search results: {len(docs)}")  # Display the number of results
 
         for index, doc in enumerate(docs, 1):
+            # Debug: Check each loop iteration
+            print(f"Displaying result {index}")
             st.write(f"Result {index}:")
-            st.write(doc.page_content[:2000])  # Display the first 2000 characters of each search result
+            st.write(doc.page_content)  # Display each search result
             st.write("---")
     else:
         st.write("No results found.")
+        
+    for i in range(min(3, len(docs))):
+        st.write(docs[i].page_content[:500])
