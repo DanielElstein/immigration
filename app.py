5cd3dd15aabfc10cb6a7da440f3a4f985d6e9ce9
DanielElstein <52753872+DanielElstein@users.noreply.github.com>
2023-05-19 16:08:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ custom_css = """
         overflow: hidden;
     }
     .stApp {
-        background-color: #FFFFB3;
+        background-color: #FFFFE6;
         color: #000;
     }
     .stApp h1 a, .stApp h2 a, .stApp h3 a, .stApp h4 a, .stApp h5 a, .stApp h6 a,
